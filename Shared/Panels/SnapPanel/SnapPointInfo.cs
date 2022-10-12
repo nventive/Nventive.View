0fe47255921537c437c91e69893316853181be90
Gabriel Chouinard Létourneau <gabriel.chouinard@nventive.com>
2022-10-12 10:34:17 -0400

    feat: add dotnet 6 support

---
+++ Shared/Panels/SnapPanel/SnapPointInfo.cs
@@ -0,0 +1,22 @@
+﻿#if WINDOWS_UWP
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nventive.View.Controls
+{
+	public class SnapPointInfo
+	{
+		public SnapPointInfo(float nearSnapPoint, float centerSnapPoint, float farSnapPoint)
+		{
+			this.NearSnapPoint = nearSnapPoint;
+			this.CenterSnapPoint = centerSnapPoint;
+			this.FarSnapPoint = farSnapPoint;
+		}
+
+		public float NearSnapPoint { get; }
+		public float CenterSnapPoint { get; }
+		public float FarSnapPoint { get; }
+	}
+}
+#endif
---
+++ src/View.Uno.WinUI/Internal/MainDispatcher/MainDispatcherScheduler.Windows.cs
@@ -0,0 +1,40 @@
+﻿#if WINDOWS10_0_18362_0
+// TODO: Implement a CoreDispatcherScheduler equivalence for Windows .NET 6 https://github.com/nventive/Nventive.View/issues/50
+using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+
+namespace Nventive.View
+{
+	internal partial class MainDispatcherScheduler : IScheduler
+	{
+		private IDisposable ScheduleCore<TState>(TState state, TimeSpan dueTime, Func<IScheduler, TState, IDisposable> action)
+		{
+			if (dueTime <= TimeSpan.Zero)
+			{
+				return Schedule(state, action);
+			}
+
+			var disposable = new SerialDisposable();
+			var timer = new System.Threading.Timer(t =>
+			{
+				if (!disposable.IsDisposed)
+				{
+					try
+					{
+						disposable.Disposable = action(this, state);
+					}
+					catch (Exception)
+					{
+						disposable.Dispose();
+					}
+				}
+			});
+
+			timer.Change((int)dueTime.TotalMilliseconds, System.Threading.Timeout.Infinite);
+
+			return disposable;
+		}
+	}
+}
+#endif
